7814cad56bbaea4297bbf00e5cfdd54fb357254f
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2020-12-15 18:25:04 +0200

    Set jwt token

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,7 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.VUE_APP_API,
 });
+
+const access_token = localStorage.getItem("access_token");
+if (access_token) {
+  api.defaults.headers.common.Authorization = `Bearer ${access_token}`;
+} else {
+  api.defaults.headers.common.Authorization = null;
+}
 
 export default api;

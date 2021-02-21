523d03d89a097b8573515b35dd34bd1037945fc3
Evgheni Calcutin <evgheni.calcutin@gmail.com>
2021-02-21 14:45:11 +0300

    Adding bearer auth

--- src/store/app/actions.js
+++ src/store/app/actions.js
@@ -1,3 +1,6 @@
+import api from '@/plugins/api';
+import { getToken } from '../../utils/auth'
+
 import * as appActions from "./action-types";
 import { READ_PRODUCTS } from "@/store/products/action-types";
 import { READ_CATEGORIES } from "@/store/categories/action-types";
@@ -5,6 +8,11 @@ import { READ_STOCKS } from "@/store/stocks/action-types";
 
 export default {
   async [appActions.APP_INIT]({ dispatch }) {
+    const token = getToken();
+    if (token) {
+      api.defaults.headers.common.Authorization = `Bearer ${token}`;
+    }
+
     dispatch(READ_PRODUCTS, {});
     dispatch(READ_CATEGORIES, {});
     dispatch(READ_STOCKS, {});
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -34,7 +34,6 @@ export const isAuthenticated = () => {
       return true;
     } else {
       removeToken();
-      // remove auth header
       return false;
     }
   }
